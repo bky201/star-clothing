af2df89e87849f22da2adbcc6ac916c3d91da7c5
bek <bekrye@gmail.com>
2023-11-04 01:10:27 +0100

    Add styles to navigating link and logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
+
+
+const Shop = () => {
+  return <h1>I'm the Shop page</h1>;
+}
 
 const App = () => {
   return (
   <Routes>
-    <Route path="/" element={<Home />} />
+    <Route path="/" element={<Navigation />} >
+      <Route index element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+    </Route>
   </Routes>
   )
 };
