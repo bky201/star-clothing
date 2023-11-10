d6bc942e6ca9523c2c9a6794971a158982ee1a37
bek <bekrye@gmail.com>
2023-11-11 00:37:42 +0100

    Create Business Logic in our Selectors

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,1 +1,28 @@
-export const selectCategoriesMap = (state) => state.categories.categoriesMap;
+// export const selectCategoriesMap = (state) => {
+//     const categoriesMap = state.categories.categories.reduce(
+//       (acc, { title, items }) => {
+//         acc[title.toLowerCase()] = items;
+//         return acc;
+//       },
+//       {}
+//     );
+//     return categoriesMap;
+//   };
+
+import { createSelector } from 'reselect';
+
+const selectCategoriesState = state => state.categories;
+
+export const selectCategoriesMap = createSelector(
+  [selectCategoriesState],
+  categoriesState => {
+    const categoriesMap = categoriesState.categories.reduce(
+      (acc, { title, items }) => {
+        acc[title.toLowerCase()] = items;
+        return acc;
+      },
+      {}
+    );
+    return categoriesMap;
+  }
+);
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -47,7 +47,10 @@ export const signInWithGoogleRedirect = () =>
 
 export const db = getFirestore();
 
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
   const collectionRef = collection(db, collectionKey);
   const batch = writeBatch(db);
 
@@ -65,14 +68,11 @@ export const getCategoriesAndDocuments = async () => {
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
-  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot.data();
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
 
-  return categoryMap;
-}
+  
+
+};
 
 export const createUserDocumentFromAuth = async (
   userAuth,
