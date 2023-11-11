dc0514e48d0a57f533b7fcb2ecec85aa002d6ddd
bek <bekrye@gmail.com>
2023-11-11 22:55:43 +0100

    Fetch Categories Async Thunk to Saga

--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -2,8 +2,6 @@ import { CATEGORIES_ACTION_TYPES } from "./categories.types";
 
 import { createAction } from "../../utils/reducer/reducer.utils";
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-
 export const setCategories = (categoriesArray) =>
   createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
 
@@ -19,14 +17,4 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
 export const fetchCategoriesFailure = (error) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
 
-  export const fetchCategoriesStartAsync = () => {
-    return async (dispatch) => {
-      dispatch(fetchCategoriesStart());
-      try {
-        const categoriesArray = await getCategoriesAndDocuments('categories');
-        dispatch(fetchCategoriesSuccess(categoriesArray));
-      } catch (error) {
-        dispatch(fetchCategoriesFailure(error));
-      }
-    };
-  };
+
--- src/store/store.js
+++ src/store/store.js
@@ -3,16 +3,20 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import logger from "redux-logger";
-import thunk from 'redux-thunk';
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger, thunk].filter(
-  Boolean
-);
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWares = [
+  process.env.NODE_ENV === "development" && logger,
+  sagaMiddleware,
+].filter(Boolean);
 
 const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
+  (process.env.NODE_ENV !== "production" &&
     window &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
@@ -25,7 +29,6 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
@@ -33,5 +36,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
